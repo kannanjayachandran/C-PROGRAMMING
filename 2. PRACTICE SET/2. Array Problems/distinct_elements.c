3ea3e145b3c6ca595238fecb0d1852de53934efe
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-02-06 22:42:06 +0530

    update

---
+++ 2. PRACTICE SET/2. Array Problems/distinct_elements.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int isDistinct(int[], int);
+
+int main(void)
+{
+    int arr[] = {1, 3, 5, 3, 1, 5, 6, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = isDistinct(arr, n);
+    printf("There are %d distinct elements in the given array.", result);
+
+    return 0;
+}
+
+int isDistinct(int arr[], int n)
+{
+    int count = 0;
+    int unique = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        unique = 1;
+        for (int j = i - 1; j >= 0; --j)
+        {
+            if (arr[i] == arr[j])
+            {
+                unique = 0;
+                break;
+            }
+        }
+        if (unique == 1)
+            count++;
+    }
+
+    return count;
+}
