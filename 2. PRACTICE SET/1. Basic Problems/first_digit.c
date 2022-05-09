db46e2464ae3978a76e0a6f44cc2f1a5ce70c139
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-05-09 21:52:23 +0530

    added program

--- 2. PRACTICE SET/1. Basic Problems/first_digit.c
+++ 2. PRACTICE SET/1. Basic Problems/first_digit.c
@@ -7,7 +7,7 @@ int main(void)
     int num;
     scanf("%d", &num);
 
-    while (num > 10)
+    while (num >= 10)
     {
         num /= 10;
     }
@@ -16,3 +16,7 @@ int main(void)
 
     return 0;
 }
+/*
+    If the number is greater than 10 we have to divide it by 10 until it become a single digit.
+    If the number is less than 10, we already has the first digit.
+*/
